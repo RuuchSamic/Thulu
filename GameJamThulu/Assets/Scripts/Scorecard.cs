6172d86544a945cf4c187375593ec73466116328
matt19271 <matthew.j.berrios@gmail.com>
2019-03-30 14:52:08 -0700

    main game update

--- GameJamThulu/Assets/Scripts/Scorecard.cs
+++ GameJamThulu/Assets/Scripts/Scorecard.cs
@@ -7,7 +7,9 @@ public class Scorecard : MonoBehaviour
 
     public NoteContainer nC1;
     public NoteContainer2 nC2;
-    private int gameScore;
+    public GameObject Player1;
+    public GameObject Player2;
+    public int gameScore;
 
     // Start is called before the first frame update
     void Start()
@@ -20,5 +22,7 @@ public class Scorecard : MonoBehaviour
     {
         gameScore = nC2.player2Count - nC1.player1Count;
         print("Total game score: " + gameScore);
+
+        //if(gameScore)
     }
 }
