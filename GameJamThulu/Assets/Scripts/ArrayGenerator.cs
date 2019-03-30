6313cc4e50b23aac2de8bf24138100db9ae88723
JTanXsol <tjohnnaidi@gmail.com>
2019-03-29 20:49:31 -0700

    Edited script (WASD, updownleftright)

--- GameJamThulu/Assets/Scripts/ArrayGenerator.cs
+++ GameJamThulu/Assets/Scripts/ArrayGenerator.cs
@@ -4,11 +4,41 @@ using UnityEngine;
 
 public class ArrayGenerator : MonoBehaviour
 {
+    public KeyCode[] player1 = new KeyCode[10];
+    public KeyCode[] player2 = new KeyCode[10];
+    private int randomKey;
+    //private Random randomGenerator = new Random();
+
+
     // Start is called before the first frame update
     void Start()
     {
-        char[] player1 = new char[10];
-        char[] player2 = new char[10];
+        for (int i=0; i < 10; i++)
+        {
+            randomKey = Random.Range(0,4);
+
+            if (randomKey == 0)
+            {
+                player1[i] = KeyCode.W;
+                player2[i] = KeyCode.UpArrow;
+            }
+            else if (randomKey == 1)
+            {
+                player1[i] = KeyCode.A;
+                player2[i] = KeyCode.LeftArrow;
+            }
+            else if(randomKey == 2)
+            {
+                player1[i] = KeyCode.S;
+                player2[i] = KeyCode.DownArrow;
+            }
+            else
+            {
+                player1[i] = KeyCode.D;
+                player2[i] = KeyCode.RightArrow;
+            }
+        }
+
     }
 
     // Update is called once per frame
