e809ec43a19407db78dded01acf8cb7f514d2ae8
matt19271 <matthew.j.berrios@gmail.com>
2019-03-30 15:01:44 -0700

    push by request by marcus. unfinished srcipts

---
+++ GameJamThulu/Assets/SpriteResizer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteResizer : MonoBehaviour
+{
+
+    Player1Select player1;
+    Player2Select player2;
+    Scorecard scoreKeep;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    //    int currentScore = scoreKeep.gameScore;
+        
+    //    if( )
+    //    {
+
+    //    }
+    //    else if{
+
+    //    }
+    //    else if()
+    //    {
+
+    //    }
+
+    }
+}
