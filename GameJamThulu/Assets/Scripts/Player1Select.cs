65056c20b8ddd24b72c24eaa9560daa5c9d0b1c8
Marcus Chiu <marcus.s.chiu@gmail.com>
2019-03-30 14:33:31 -0700

    Completed the Player Selection screens

---
+++ GameJamThulu/Assets/Scripts/Player1Select.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player1Select : MonoBehaviour
+{
+    public static int playerOne;
+    public void Player1Choose(int charNum)
+    {
+        playerOne = charNum;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
