2d6bf4ce808062b7378e951334a06cdec74b6953
Marcus Chiu <marcus.s.chiu@gmail.com>
2019-03-30 10:23:25 -0700

    Updated Sprites to reflect the keys that need to be pushed.

--- GameJamThulu/Assets/Scripts/StackGenerator.cs
+++ GameJamThulu/Assets/Scripts/StackGenerator.cs
@@ -10,6 +10,14 @@ public class StackGenerator : MonoBehaviour
     public List<GameObject> player1StackList = new List<GameObject>();
     public List<GameObject> player2StackList = new List<GameObject>();
     public GameObject noteObj;
+    public GameObject play1Up;
+    public GameObject play1Rt;
+    public GameObject play1Lt;
+    public GameObject play1Dn;
+    public GameObject play2Up;
+    public GameObject play2Rt;
+    public GameObject play2Lt;
+    public GameObject play2Dn;
     public Vector3 vectorShift = new Vector3(4, 0, 0);
 
     // Start is called before the first frame update
@@ -28,22 +36,30 @@ public class StackGenerator : MonoBehaviour
 
             if (randomKey == 0)
             {
+                go1.GetComponent<SpriteRenderer>().sprite = play1Up.GetComponent<SpriteRenderer>().sprite;
                 go1.GetComponent<HitNote>().key = KeyCode.W;
+                go2.GetComponent<SpriteRenderer>().sprite = play2Up.GetComponent<SpriteRenderer>().sprite;
                 go2.GetComponent<HitNote>().key = KeyCode.UpArrow;
             }
             else if (randomKey == 1)
             {
+                go1.GetComponent<SpriteRenderer>().sprite = play1Lt.GetComponent<SpriteRenderer>().sprite;
                 go1.GetComponent<HitNote>().key = KeyCode.A;
+                go2.GetComponent<SpriteRenderer>().sprite = play2Lt.GetComponent<SpriteRenderer>().sprite;
                 go2.GetComponent<HitNote>().key = KeyCode.LeftArrow;
             }
             else if (randomKey == 2)
             {
+                go1.GetComponent<SpriteRenderer>().sprite = play1Dn.GetComponent<SpriteRenderer>().sprite;
                 go1.GetComponent<HitNote>().key = KeyCode.S;
+                go2.GetComponent<SpriteRenderer>().sprite = play2Dn.GetComponent<SpriteRenderer>().sprite;
                 go2.GetComponent<HitNote>().key = KeyCode.DownArrow;
             }
             else
             {
+                go1.GetComponent<SpriteRenderer>().sprite = play1Rt.GetComponent<SpriteRenderer>().sprite;
                 go1.GetComponent<HitNote>().key = KeyCode.D;
+                go2.GetComponent<SpriteRenderer>().sprite = play2Rt.GetComponent<SpriteRenderer>().sprite;
                 go2.GetComponent<HitNote>().key = KeyCode.RightArrow;
             }
 
