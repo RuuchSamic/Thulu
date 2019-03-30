69b936e5f4831f948b4dbd4c734e031f0346aa39
Marcus Chiu <marcus.s.chiu@gmail.com>
2019-03-30 13:22:58 -0700

    Blocks no longer rotate

--- GameJamThulu/Assets/Scripts/HitNote.cs
+++ GameJamThulu/Assets/Scripts/HitNote.cs
@@ -7,11 +7,13 @@ public class HitNote : MonoBehaviour
     public KeyCode key;
     public GameObject note;
     public StackGenerator stackGen;
+    private Rigidbody2D myBody;
 
     // Start is called before the first frame update
     void Start()
     {
         //keyGen.player1
+        transform.rotation = Quaternion.Euler(0, 0, 0);
     }
 
     // Update is called once per frame
